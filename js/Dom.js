02b9d8730f8427aecb702f8f5c95967702d17446
abirahmmed12 <abirahmedchowdhury12@gmail.com>
2023-08-21 19:39:05 +0600

    adding final

--- js/Dom.js
+++ js/Dom.js
@@ -1,74 +1,68 @@
-let total = 0
+let total = 0;
+let counter = 1;
 function handleclickbtn(target) {
-    
+
     const productName = target.querySelector('.card-body h2').innerText;
-    const productPrice = target.querySelector('.card-body span').innerText
-    
-    
-
-    
-    const getselectedItem = document.getElementById('selecteditem')
-    const li =document.createElement('li')
-    li.innerText= productName
-    getselectedItem.appendChild(li)
-  total = parseFloat(total)+ parseFloat(productPrice)
-  document.getElementById('total').innerText=total
-
-  const gettingPurchaseBtn = document.getElementById('Purchase');
-
-  // Function to update the Purchase button's state
-  function updatePurchaseButton() {
-      if (total > 0) {
-          gettingPurchaseBtn.removeAttribute('disabled');
-      } else {
-          gettingPurchaseBtn.setAttribute('disabled', 'true');
-      }
-      
-  }
-  updatePurchaseButton()
-  const gettingapplyBtn = document.getElementById('apply');
-
-  
-  function updateApplyButton() {
-      if (total <= 200 ) {
-          gettingapplyBtn.setAttribute('disabled', 'true');
-      } else {
-          gettingapplyBtn.removeAttribute('disabled');
-      }
-  }
-
-  updateApplyButton(); 
+    const productPrice = target.querySelector('.card-body span').innerText;
 
 
+    const getSelectedItem = document.getElementById('selecteditem');
+    const orderedList = document.getElementById('orderedList');
 
-  
+    getSelectedItem.innerText = productName;
+    const li = document.createElement('li');
+    li.innerText = counter + '. ' + productName;
+    orderedList.appendChild(li);
+    counter++;
 
+    total = parseFloat(total) + parseFloat(productPrice);
+    document.getElementById('total').innerText = total.toFixed(2);
 
+    const gettingPurchaseBtn = document.getElementById('Purchase');
+
+    function updatePurchaseButton() {
+        if (total > 0) {
+            gettingPurchaseBtn.removeAttribute('disabled');
+        } else {
+            gettingPurchaseBtn.setAttribute('disabled', 'true');
+        }
+
+    }
+    updatePurchaseButton()
+    const gettingapplyBtn = document.getElementById('apply');
+    function updateApplyButton() {
+        if (total < 200) {
+            gettingapplyBtn.setAttribute('disabled', 'true');
+        } else {
+            gettingapplyBtn.removeAttribute('disabled');
+        }
+    }
+    updateApplyButton();
 }
 
 function getting() {
     const gettinginput = document.getElementById('input');
-    const gettingvalue = gettinginput.value;
-    const ulitmatetotal = total - (total * 0.20)
-
+    const gettingvalue = gettinginput.value.trim();
+    const ulitmatetotal = total - (total * 0.20);
     if (gettingvalue === 'SELL200') {
         const getdiscount = document.getElementById('discount');
         getdiscount.innerText = ulitmatetotal.toFixed(2)
+    }else {
+        gettingapplyBtn.setAttribute('disabled', 'true');
     }
     const moneysave = document.getElementById('savemoney');
     const savedAmount = (parseFloat(total.toFixed(2)) - parseFloat(ulitmatetotal.toFixed(2))).toFixed(2);
     moneysave.innerText = savedAmount;
 }
-function gethomebtn(){
-    document.getElementById('total').innerHTML=''
-    document.getElementById('selecteditem').innerHTML=''
-    document.getElementById('savemoney').innerHTML=''
-    document.getElementById('discount').innerHTML=''
+function gethomebtn() {
+    document.getElementById('total').innerHTML = ''
+    document.getElementById('selecteditem').innerHTML = ''
+    document.getElementById('savemoney').innerHTML = ''
+    document.getElementById('discount').innerHTML = ''
     document.getElementById('input').value = ''
     document.getElementById('apply').setAttribute('disabled', 'disabled');
     document.getElementById('Purchase').setAttribute('disabled', 'disabled');
 }
 
- 
 
-       
+
